54fa76bf550b28121329d1331ab4561370dd7172
Y. Isoda <y.isoda.3953@gmail.com>
2022-04-29 13:29:57 +0900

    add blank test and fix Actions

---
+++ IgorDataFormatter.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IgorTextFormatter.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public async Task TestMethod1Async()
+        {
+        }
+    }
+}
